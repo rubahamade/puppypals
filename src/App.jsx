d4f75b5af8d015e0414d85e361d9fb8a78acbb63
Ruba <ruba@Rubas-MacBook-Pro.local>
2023-12-04 16:59:54 -0800

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-
-import './App.css'
 import {puppyList} from './data.js'
+import { useState } from 'react'
+import './App.css'
+
 
 function App() {
-  console.log(puppy.list)
+  
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList:", puppyList);
+
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => 
+    pup.id === featPupId
+  )
+  console.log(featuredPup)
+
+
 
   return (
-    <>
-     
-    </>
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+          return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+            </p>
+        })
+      }
+
+
+      {
+        featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+      )}
+    </div>
   )
 }
 
